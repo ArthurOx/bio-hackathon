17ca51bc73ba9276a19b30a7a27be02a4087dfa3
meship <101132768+meship@users.noreply.github.com>
2023-01-15 14:31:08 +0200

    update2

--- hackton.py
+++ hackton.py
@@ -1,34 +1,49 @@
-import numpy as np
-from itertools import groupby
-from Bio import AlignIO
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Align.Applications import MuscleCommandline
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, TreeConstructor, MultipleSeqAlignment, \
+	DistanceCalculator
+from Bio.Phylo import draw
+from Bio import SeqIO, AlignIO, Seq
+import os
 
-def fastaread(fasta_name):
-	"""
-    Read a fasta file. For each sequence in the file, yield the header and the actual sequence.
-    In Ex1 you may assume the fasta files contain only one sequence.
-    You may keep this function, edit it, or delete it and implement your own reader.
-    """
-	f = open(fasta_name)
-	faiter = (x[1] for x in groupby(f, lambda line: line.startswith(">")))
-	for header in faiter:
-		header = next(header)[1:].strip()
-		seq = "".join(s.strip() for s in next(faiter))
-		yield header, seq
 
-def create_model():
-	alignment = list(fastaread('Data/world1.fasta'))
-	sequences = []
-	for header, sequence in alignment:
-	longest_length = max(len(s) for s in alignment)
-	padded_sequences = [s.ljust(longest_length, '-') for s in sequences]
-	records = (SeqRecord(Seq(s)) for s in padded_sequences)
-	SeqIO.write(records, "example.fasta", "fasta")
+def label_func(x):
+	return None
 
-	j =3
 
-if __name__ == '__main__':
-	create_model()
+def padd_sequnces(input_file):
+	global record
+	records = SeqIO.parse(input_file, 'fasta')
+	records = list(records)  # make a copy, otherwise our generator
+	# is exhausted after calculating maxlen
+	maxlen = max(len(record.seq) for record in records)
+	# pad sequences so that they all have the same length
+	for record in records:
+		if len(record.seq) != maxlen:
+			sequence = str(record.seq).ljust(maxlen, '.')
+			record.seq = Seq.Seq(sequence)
+	assert all(len(record.seq) == maxlen for record in records)
+	# write to temporary file and do alignment
+	output_file = '{}_padded.fasta'.format(os.path.splitext(input_file)[0])
+	with open(output_file, 'w') as f:
+		SeqIO.write(records, f, 'fasta')
+
+
+def find_duplicates(file):
+	with open(file, "r") as f:
+		lines = f.readlines()
+		names = []
+		for line in lines:
+			if line.startswith(">"):
+				names.append(line)
+
+	names_set = set(names)
+	for name in names_set:
+		if names.count(name) > 1:
+			print(name)
+
+
+if __name__ == "__main__":
+	data = "world_padded.fasta"
+	alignment = AlignIO.read(data, "fasta")
+	tree_constructor = DistanceTreeConstructor(distance_calculator=DistanceCalculator('identity'))
+	tree = tree_constructor.build_tree(alignment)
+	draw(tree=tree, label_func=label_func)
